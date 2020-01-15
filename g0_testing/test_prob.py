fb9e117078378acd0a73ae16736f9699a715e6cd
IArchondo <i.archondo@outlook.com>
2020-01-15 21:36:20 +0100

    Generated general structure in tutorial to calculate a player's discrete probability distribution

--- g0_testing/test_prob.py
+++ g0_testing/test_prob.py
@@ -1,5 +1,3 @@
-!pip install seaborn
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -19,13 +17,13 @@ data_uniform
 ## normal distribution
 from scipy.stats import norm
 
-data_normal = norm.rvs(size=10,loc=0,scale=10)
+data_normal = norm.rvs(size=10, loc=0, scale=10)
 
 data_normal
 
-player_fppm = player.loc[player['MIN']>0,:]['FPPM']
+player_fppm = player.loc[player["MIN"] > 0, :]["FPPM"]
 
-ax = sns.distplot(player_fppm,kde=True,bins=100,color='skyblue')
+ax = sns.distplot(player_fppm, kde=True, bins=100, color="skyblue")
 
 
 ##### use http://www.insightsbot.com/fitting-probability-distributions-with-python/
@@ -36,8 +34,8 @@ import matplotlib.pyplot as plt
 
 
 class Distribution(object):
-    def __init__(self,dist_names_list=[]):
-        self.dist_names = ["norm",'lognorm','expon','pareto']
+    def __init__(self, dist_names_list=[]):
+        self.dist_names = ["norm", "lognorm", "expon", "pareto"]
         self.dist_results = []
         self.params = {}
 
@@ -53,38 +51,36 @@ class Distribution(object):
         for dist_name in self.dist_names:
             dist = getattr(scipy.stats, dist_name)
             param = dist.fit(y)
-            
+
             self.params[dist_name] = param
-            #Applying the Kolmogorov-Smirnov test
-            D, p = scipy.stats.kstest(y, dist_name, args=param);
-            self.dist_results.append((dist_name,p))
-        #select the best fitted distribution
-        sel_dist,p = (max(self.dist_results,key=lambda item:item[1]))
-        #store the name of the best fit and its p value
+            # Applying the Kolmogorov-Smirnov test
+            D, p = scipy.stats.kstest(y, dist_name, args=param)
+            self.dist_results.append((dist_name, p))
+        # select the best fitted distribution
+        sel_dist, p = max(self.dist_results, key=lambda item: item[1])
+        # store the name of the best fit and its p value
         self.DistributionName = sel_dist
         self.PValue = p
 
         self.isFitted = True
-        return self.DistributionName,self.PValue
+        return self.DistributionName, self.PValue
 
-    def Random(self, n = 1):
+    def Random(self, n=1):
         if self.isFitted:
             dist_name = self.DistributionName
             param = self.params[dist_name]
-            #initiate the scipy distribution
+            # initiate the scipy distribution
             dist = getattr(scipy.stats, dist_name)
             return dist.rvs(*param[:-2], loc=param[-2], scale=param[-1], size=n)
         else:
-            raise ValueError('Must first run the Fit method.')
+            raise ValueError("Must first run the Fit method.")
 
-    def Plot(self,y):
+    def Plot(self, y):
         x = self.Random(n=len(y))
-        plt.hist(x, alpha=0.5, label='Fitted')
-        plt.hist(y, alpha=0.5, label='Actual')
-        plt.legend(loc='upper right')
+        plt.hist(x, alpha=0.5, label="Fitted")
+        plt.hist(y, alpha=0.5, label="Actual")
+        plt.legend(loc="upper right")
 
-
-# fit discrete distribution?
 
 dst = Distribution()
 
@@ -93,10 +89,34 @@ dst.Fit(player_fppm)
 dst.Plot(player_fppm)
 
 
-import seaborn as sns 
-ax = sns.distplot(dst.Random(n=1000),kde=True,bins=100,color='skyblue')
+import seaborn as sns
 
-player_fppm
+ax = sns.distplot(dst.Random(n=1000), kde=True, bins=100, color="skyblue")
 
-scipy.stats.norm.fit(player_fppm)
+# fit discrete distribution?
+from scipy.stats import multinomial
+
+p = [0.3, 0.3, 0.4]
+k = 1
+
+dist = multinomial(n=k, p=p)
+
+dist.pmf([1, 0, 0])
+
+rng = np.random.default_rng()
+
+rng.multinomial(1, [0.1, 0.1, 0.8], size=10)
+
+rng.multinomial(20, [1 / 6.0] * 6, size=1)
+array([[4, 1, 7, 5, 2, 1]])  # random
+
+
+## mock transformation from list to probabilities
+
+# probable FPPMs
+import numpy as np
+
+possible_fppm = np.arange(0, 3.1, 0.1)
+
+from g1_data_gathering.DataFetcher import DataFetcher
 
--- tutorial.py
+++ tutorial.py
@@ -10,7 +10,9 @@ datafetch = DataFetcher()
 
 player = datafetch.get_player_clean_gamelog("Christian Wood")
 
-#datafetch.get_player_performance("Nikola Jokic",60)
+player
+
+# datafetch.get_player_performance("Nikola Jokic",60)
 
 matchupreader = MatchupReader()
 
@@ -20,3 +22,55 @@ matchupcalculator = MatchupCalculator(out)
 
 match = matchupcalculator.process_game(matchupcalculator.input_matchup)
 
+
+## mock transformation from list to probabilities
+
+# probable FPPMs
+import numpy as np
+
+possible_fppm = np.arange(0, 3.1, 0.1)
+possible_fppm = [round(value, 2) for value in possible_fppm]
+
+from g1_data_gathering.DataFetcher import DataFetcher
+from collections import Counter
+
+datafetch = DataFetcher()
+player = datafetch.get_player_clean_gamelog("Giannis Antetokounmpo")
+
+fppms = player[player["MIN"] > 0]["FPPM"]
+
+# replace all negative values with 0
+fppms = [0 if value < 0 else value for value in fppms]
+fppms = [3 if value > 3 else value for value in fppms]
+
+# round to nearest 0.1
+fppms = [round(value * 10) / 10 for value in fppms]
+
+game_count = len(fppms)
+
+# count ocurrences
+occ_counter = Counter(fppms)
+
+# insert occurrences into all posible occurrences
+general_occ = {}
+for pos in possible_fppm:
+    if round(pos, 1) in occ_counter.keys():
+        value = occ_counter[pos]
+    else:
+        value = 0
+    general_occ[round(pos, 1)] = value
+
+probabilities = [general_occ[key] / game_count for key in general_occ.keys()]
+
+rng = np.random.default_rng()
+
+simulation = rng.multinomial(1, probabilities, size=100)
+
+outcomes = [
+    possible_fppm[np.where(simulation[key] == True)[0][0]].sum()
+    for key in range(len(simulation))
+]
+
+outcomes.sort()
+
+Counter(outcomes)
